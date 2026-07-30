66b404df0cf0d12983809ddf51357dd00b8ca4fb
Bonthewad_Dattatri <sdbrothers333@gmail.com>
2026-07-30 10:20:36 +0530

    Time: 27 ms (46.41%), Space: 69.4 MB (56.4%) - LeetHub

---
+++ 2390-removing-stars-from-a-string/2390-removing-stars-from-a-string.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var removeStars = function(s) {
+
+    const stack =[]
+    for ( const char of s){
+        if (char !== "*"){
+            stack.push(char)
+        }else{
+            stack.pop()
+        }
+    }
+    return stack.join('')
+
+    
+};
