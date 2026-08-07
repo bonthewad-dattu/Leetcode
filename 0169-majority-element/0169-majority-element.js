7dbeea44fa1e591c0aefb0a7f458fe249709a5e2
Bonthewad_Dattatri <sdbrothers333@gmail.com>
2026-08-07 14:41:29 +0530

    Time: 1955 ms (5.07%), Space: 56 MB (84.42%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var majorityElement = function(nums) {
+    for (let i = 0;i<nums.length;i++){
+        count = 0
+        for(let j = 0 ; j<nums.length;j++){
+            if (nums[i] == nums[j]){
+                count ++;
+            
+            if (count > nums.length / 2){
+                return nums[i]
+            }
+            }
+        }
+    }
+    
+};
